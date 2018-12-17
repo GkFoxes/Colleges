5e1a575a7445423911c808e933755bbac59851f1
GkFoxes <dima25_matveenko@icloud.com>
2018-12-17 18:04:41 +0300

    Add LabOne and LabTwo reports in TSI

---
+++ CourseThree/TSI/LabTwo.cpp
@@ -0,0 +1,24 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <windows.h>
+
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+
+	int width = GetSystemMetrics(SM_CXSCREEN);
+	int height = GetSystemMetrics(SM_CYSCREEN);
+	
+	HDC dc = GetDC(NULL);
+	int bitsPerPixel = GetDeviceCaps(dc, BITSPIXEL);
+
+	cout << "Разрешение экрана: " << width << "/" << height << "\n";
+	cout << "Глубина цвета: " << bitsPerPixel << "\n";
+	cout << "\n";
+
+	cout << "Размер кадра: " << "\n";
+	cout << width * height * bitsPerPixel / 8 << " Байт" << "\n";
+	cout << width * height * bitsPerPixel / 8 / 1024 << " KБайт" << "\n";
+	cout << width * height * bitsPerPixel / 8 / 1024 / 1024 << " МБайт" << "\n";
+}
